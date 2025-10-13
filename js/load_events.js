c8af192b263ecf2a3f935a24e5c9235fa8b081a4
Liechie-idk <johnschinn123@outlook.com>
2025-10-13 15:33:00 -0700

    Added Events List

--- js/load_events.js
+++ js/load_events.js
@@ -1,19 +1,23 @@
-function loadItems() {
-    
-    fetch('js/event_list.txt')
-    .then(response => response.text())
-    .then(textString => {
-        console.log(textString);
-        list = textString.split('\n');
-        console.log(list.length);
-        for(var i; i = (list.length/4); i++)
-        {
-            var input1 = list[(4*i)+1];
-            var input2 = list[(4*i)+2];
-            var input3
-            var newBox = document.createElement("div");
-            newBox.className = "scroll_box-item"
-            newBox.innerHTML = '<div class="scroll_box-item-date"' + input1 + '<br> 08 </div><p>Picture Re-Take  Day</p>';
-        }
-    })
-}
+const parentBox = document.getElementById("scroll");
+console.log(parentBox);
+var boxList = [];
+
+fetch('js/event_list.txt')
+.then(response => response.text())
+.then(textString => {
+    // console.log(textString);
+    const list = textString.split('\n');
+    console.log(list.length);
+    for(var i = 0; i < (list.length/4); i++)
+    {
+        
+        var input1 = list[(4*i)];
+        var input2 = list[(4*i)+1];
+        var input3 = list[(4*i)+2];
+
+        var newBox = '<div class="scroll_box-item"> <div class="scroll_box-item-date"> ' + input1 + ' <br> ' + input2 +' </div> <p>' + input3 + '</p> </div>'
+                
+        parentBox.innerHTML += newBox;
+        console.log(boxList);
+    }
+})
